f75abea834739b2bd4651e123a910bfd4153d28b
apoorv0070 <67070413+apoorv0070@users.noreply.github.com>
2025-12-02 00:33:26 +0530

    Update urls.py

--- menumind-backend/menumind/urls.py
+++ menumind-backend/menumind/urls.py
@@ -1,4 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-urlpatterns = [path('admin/', admin.site.urls), path('api/', include('app.urls'))]
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "Backend Running", "message": "MenuMind API Live"})
+urlpatterns = [path('', home),path('admin/', admin.site.urls), path('api/', include('app.urls'))]
